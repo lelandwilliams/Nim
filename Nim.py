975218eb5b5fd38901c50edb5fc4a3d89dc37137
Lee Williams <will4379@umn.edu>
2016-07-11 13:13:23 -0500

    Initial Commit

---
+++ Nim.py
@@ -0,0 +1,15 @@
+
+
+preperiod = (0,0,0) # usually labelled 'R'
+period = (1,1,1) # usually labelled 'D'
+
+outcomes ={}
+outcomes[(0,0,0)] = 'P'
+begin = (0,0,0) 
+
+def Nim():
+    correct_period = False
+    while(not dimension_hold):
+
+
+
