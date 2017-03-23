5b38da25f4af47cd6dea84d3e7b786d2843acf93
Lee Williams <will4379@umn.edu>
2017-03-22 19:45:04 -0500

    (NimBase) corrected functionname in getSlice()

--- NimBase.py
+++ NimBase.py
@@ -108,7 +108,7 @@ class NimBase(NimTuples):
 
         # set lower dimensions of cur_tuple to 0
         for i in range(1, dimension):
-            cur_tuple = self.setXtoY(cur_tuple, i, 0)
+            cur_tuple = self.setTupleXtoY(cur_tuple, i, 0)
 
         # remember the value of the cur_tuple in dimension. 
         cur_dimension_value = cur_tuple[dimension]
