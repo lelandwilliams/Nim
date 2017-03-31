dd755b958dead2ad5ae2f4cbb3f6b07f5750eef6
Lee Williams <will4379@umn.edu>
2017-03-31 13:49:18 -0500

    (NimTests) corrected expected output of p_positions() in test_example3

--- NimTests.py
+++ NimTests.py
@@ -26,8 +26,9 @@ class TestNim(unittest.TestCase):
         self.nim.setDimensions(2)
         self.nim.run()
         self.assertEqual(len(self.nim.p_positions()), 2)
-        self.assertEqual(self.nim.p_positions(), [(None,0,0)])
-        self.assertEqual(self.nim.rectangle, (None,2,2))
+        self.assertTrue((None,1,0) in self.nim.p_positions())
+        self.assertTrue((None,0,2) in self.nim.p_positions())
+        self.assertEqual(self.nim.rectangle, (None,2,3))
         self.assertEqual(self.nim.preperiod, (None,0,1))
 
     def test_example3_5(self):
