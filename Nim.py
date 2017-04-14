ee71753da9d468607d573b5b31a9fd969f5616e8
Lee Williams <will4379@umn.edu>
2017-04-14 14:08:05 -0500

    Initial add of new toplevel code

---
+++ Nim.py
@@ -0,0 +1,42 @@
+import NimReport
+
+class Nim(NimReport):
+    """ This is the 'top-level' or 'user-level' class of this project
+    Make sure you run this class with python3. If you are getting weird
+    messages about super(), then you probably tried to use this class
+    with python3.
+    """
+
+    def __init__(self, dimensions=3, rulecode=None, normalPlay = True):
+        # TODO make normal play an enum
+        # TODO make dimensions have a default of None
+
+        super.__init__(); # calls super class inits
+        self.rulecode = rulecode
+
+        # Test for non-full parameters, since we cant run without them
+        # Todo: Enter an interactive mode
+        if dimensions == None:
+            return True
+        self.setDimensions(dimensions)
+
+        # Test various values of rulecode to set it properly
+        # If rulecode is not set, set it to 0.3333....
+        if self.rulecode == None:
+            self.rulecode = "0."
+            while len(self.rulecode < dimensions + 2):
+                self.rulecode += "3"
+
+        # If rulecode entered as a float, convert to a string
+        if type(rulecode) == type(float()):
+            self.rulecode = rulecode.str()
+
+        # At this point rulecode should be a string. If not, 
+        # throw
+        if type(rulecode) != type(str()):
+            throw("rulecode {} is invalid. Please enter a string or a float\n".format(self.rulecode))
+
+
+
+
+
